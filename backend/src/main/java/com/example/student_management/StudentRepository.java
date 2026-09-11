7feb71fc5443d7c96bf84c1841f89d48b9f680cc
SangeethaKaliyappan <sangeethak.13112005@gmail.com>
2026-09-11 18:38:16 +0530

    Remove old Java backend

--- backend/src/main/java/com/example/student_management/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.student_management;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-    Optional<Student> findByRollNumber(String rollNumber);
-}
